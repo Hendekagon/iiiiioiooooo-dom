c817c903ca47fb247883d044c3ec72b8789a2732
Matthew Chadwick <post@celeriac.net>
2014-10-31 20:29:14 +1100

    apples

--- src/clj/iiiiioiooooo/core/structure.clj
+++ src/clj/iiiiioiooooo/core/structure.clj
@@ -1,7 +1,6 @@
 (ns iiiiioiooooo.core.structure
   (:require
     [clojure.zip :as zip]
-    ;[clojure.core.match :as m]
   )
 )
 
@@ -546,38 +545,4 @@
 (defn update! [state e]
   (swap! state (fn [s] (update s e)))
 )
-
-(comment
-(defn IDL-match [[f & r]]
-   (m/match [f]
-            ["//"] {:comment (apply str r)}
-            ["typedef"] {:typedef 123}
-            :else []
-            )
-   )
-
-   (defn parseIDL
-  ([] (parseIDL "https://www.khronos.org/registry/webgl/specs/1.0/webgl.idl"))
-  ([url] (parseIDL url (slurp url)))
-  ([url idl]
-   (parseIDL url idl
-    (->>
-      idl
-      (partition-by (-_ = \newline))
-      (map (-_ partition-by (-_ = \space)))
-      (map (-_ map (-_ apply str)))
-      ))
-  )
-  ([url idl idls]
-    {
-      :header (filter (-+ (-_ = "//") first) idls)
-      :typedefs (filter (-+ (-_ = "typedef") first) idls)
-      :blocks (partition-by (-+ (-_ = "};") first) idls)
-      :idls idls
-      :idl idl
-      :url url
-    }
-  )
-)
-   )
 
